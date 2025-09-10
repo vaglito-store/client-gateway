273c674559cd1bda4e0b036fd7b7bd941d10a8cf
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-10 02:16:20 -0500

    feat: :sparkles: add paginationdto

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,15 @@
+import { Type } from "class-transformer";
+import { IsPositive, IsOptional } from "class-validator";
+
+export class PaginationDto {
+
+    @IsPositive()
+    @IsOptional()
+    @Type(() => Number)
+    page?: number = 1;
+
+    @IsPositive()
+    @IsOptional()
+    @Type(() => Number)
+    limit?: number = 10;
+}
