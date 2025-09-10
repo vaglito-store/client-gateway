03553f0c3dc5b5668e1bc3b874cf5b71074c0830
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-10 03:23:41 -0500

    feat: :sparkles: add RpcCustomExceptionFilter

---
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -0,0 +1,32 @@
+import {
+  Catch,
+  RpcExceptionFilter,
+  ArgumentsHost,
+  ExceptionFilter,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { RpcException } from '@nestjs/microservices';
+
+@Catch(RpcException)
+export class RpcCustomExceptionFilter implements ExceptionFilter {
+  catch(exception: RpcException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    const rpcError = exception.getError();
+
+    if (
+      typeof rpcError == 'object' &&
+      'status' in rpcError &&
+      'message' in rpcError
+    ) {
+      const status = rpcError.status;
+      return response.status(status).json(rpcError);
+    }
+
+    response.status(400).json({
+      status: 400,
+      message: rpcError,
+    });
+  }
+}
