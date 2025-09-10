45ed32c92713bfe93eeb12465611aa83c7c1e289
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-10 01:13:23 -0500

    feat: :sparkles: add config for connection clientsmodule to users ms

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { USER_SERVICE, env } from 'src/config';
 
 @Module({
   controllers: [UsersController],
   providers: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: USER_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: env.user_ms_host,
+          port: env.user_ms_port,
+        },
+      },
+    ]),
+  ],
 })
 export class UsersModule {}
