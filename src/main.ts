8b908627fda45f690779f78ba2c09a062d09f4a8
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-10 02:16:49 -0500

    feat: use global pipes for validation

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from './config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-
-  const logger = new Logger('Main-Gateway')
+  const logger = new Logger('Main-Gateway');
 
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(env.port);
 
-  logger.log(`Gateway running on port ${env.port}`)
+  logger.log(`Gateway running on port ${env.port}`);
 }
 bootstrap();
