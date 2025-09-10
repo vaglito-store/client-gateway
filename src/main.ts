a2cceb5f96cdb121772c1ad189909384629e1c29
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-10 00:11:04 -0500

    feat: add global prefix 'api'

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
   const logger = new Logger('Main-Gateway')
 
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api')
+
   await app.listen(env.port);
 
   logger.log(`Gateway running on port ${env.port}`)
