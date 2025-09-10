b4a4ba9ef5dcbc6de7ed555646e40d2cbcfc8f8a
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-10 02:31:58 -0500

    feat: :sparkles: add get one user by id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,12 +10,14 @@ import {
   HttpStatus,
   Inject,
   Query,
+  BadRequestException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { USER_SERVICE } from 'src/config';
 import { ClientProxy } from '@nestjs/microservices';
 import { PaginationDto } from 'src/common';
+import { firstValueFrom } from 'rxjs';
 
 @Controller('users')
 export class UsersController {
@@ -30,18 +32,26 @@ export class UsersController {
 
   @Get()
   findAllUsers(@Query() paginationDto: PaginationDto) {
-    return this.usersClient.send({ cmd: 'find_all_users'}, paginationDto);
+    return this.usersClient.send({ cmd: 'find_all_users' }, paginationDto);
   }
 
   @Get(':id')
-  findUser(
+  async findUser(
     @Param(
       'id',
       new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
     )
     id: number,
   ) {
-    return `Get user with #${id} id`;
+    try {
+      const product = await firstValueFrom(
+        this.usersClient.send({ cmd: 'find_one_user' }, { id: id }),
+      );
+
+      return product;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
   @Patch(':id')
