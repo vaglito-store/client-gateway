2a10b38ce50fe788f5a94f8251257d649ee96030
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-10 03:32:59 -0500

    feat: add valitadion for create user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { Type } from "class-transformer";
+import { IsString, IsInt, IsPositive } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    name: string;
+
+    @IsInt()
+    @IsPositive()
+    @Type(() => Number)
+    age: number;
+
+    @IsString()
+    email: string;
+}
