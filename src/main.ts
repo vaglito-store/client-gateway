425267b16c323ebcd4c14dc6cd1b5a27f70ade94
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-10 03:25:03 -0500

    feat: :sparkles: implement RpcCustomExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from './config';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { RpcCustomExceptionFilter } from './common';
 
 async function bootstrap() {
   const logger = new Logger('Main-Gateway');
@@ -15,6 +16,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.useGlobalFilters(new RpcCustomExceptionFilter())
 
   await app.listen(env.port);
 
